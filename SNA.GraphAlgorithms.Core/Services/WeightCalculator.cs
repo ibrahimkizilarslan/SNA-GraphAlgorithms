3db37c87f75fe7b87bf9c7e9c623771d4abd19e0
İbrahim Kızılarslan <64713287+ibrahimkzilarslan@users.noreply.github.com>
2025-12-22 15:59:21 +0300

    .

---
+++ SNA.GraphAlgorithms.Core/Services/WeightCalculator.cs
@@ -0,0 +1,36 @@
+using SNA.GraphAlgorithms.Core.Models;
+using System;
+
+namespace SNA.GraphAlgorithms.Core.Services
+{
+    /// <summary>
+    /// İki node arasındaki edge weight'ini hesaplayan statik servis
+    /// </summary>
+    public static class WeightCalculator
+    {
+        /// <summary>
+        /// İki node arasındaki ağırlığı hesaplar
+        /// Formül: weight(i,j) = 1 / (1 + (Activity_i - Activity_j)^2 + (Interaction_i - Interaction_j)^2 + (Connection_i - Connection_j)^2)
+        /// </summary>
+        /// <param name="nodeA">İlk node</param>
+        /// <param name="nodeB">İkinci node</param>
+        /// <returns>Hesaplanan ağırlık değeri (0-1 arası)</returns>
+        public static double Calculate(Node nodeA, Node nodeB)
+        {
+            if (nodeA == null)
+                throw new ArgumentNullException(nameof(nodeA));
+            if (nodeB == null)
+                throw new ArgumentNullException(nameof(nodeB));
+
+            // Özellik farklarının karesi
+            double activityDiff = Math.Pow(nodeA.Activity - nodeB.Activity, 2);
+            double interactionDiff = Math.Pow(nodeA.InteractionCount - nodeB.InteractionCount, 2);
+            double connectionDiff = Math.Pow(nodeA.ConnectionCount - nodeB.ConnectionCount, 2);
+
+            // Formül: 1 / (1 + toplam_fark)
+            double weight = 1.0 / (1.0 + activityDiff + interactionDiff + connectionDiff);
+
+            return weight;
+        }
+    }
+}
