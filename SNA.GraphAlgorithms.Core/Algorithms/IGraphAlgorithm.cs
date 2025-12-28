5d2a9d23a9bc10b1619263cd749f1660f826a601
karat <karat@CIHATKARATAS>
2025-12-28 16:52:28 +0300

    .

--- SNA.GraphAlgorithms.Core/Algorithms/IGraphAlgorithm.cs
+++ SNA.GraphAlgorithms.Core/Algorithms/IGraphAlgorithm.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 namespace SNA.GraphAlgorithms.Core.Algorithms
 {
-    /// <summary>
+    
     /// Graph üzerinde çalışan algoritmaların ortak interface'i
-    /// </summary>
+    
     public interface IGraphAlgorithm
     {
-        /// <summary>
+        
         /// Algoritma adı
-        /// </summary>
+        
         string Name { get; }
 
-        /// <summary>
+        
         /// Graph üzerinde algoritmayı çalıştır
-        /// </summary>
+        
         /// <param name="graph">Üzerinde çalışılacak graph</param>
         /// <param name="startNodeId">Başlangıç düğümü ID</param>
         /// <returns>Ziyaret edilen düğümlerin ID listesi (ziyaret sırasına göre)</returns>
